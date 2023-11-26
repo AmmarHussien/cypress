45e88684e489667c6a12854079e244183904bead
ammarhussien <ammar.hussienmo97@gmail.com>
2023-11-26 11:39:27 +0200

    hover

--- cypress/integration/12-hover.spec.js
+++ cypress/integration/12-hover.spec.js
@@ -2,13 +2,21 @@ it('hover command', ()=>{
     cy.visit('cypress/index.html') 
 
     cy.get('.trigger').trigger('mouseover')
-   
 })
 
-it.only('hover command', ()=>{
+it('hover command', ()=>{
     cy.visit('cypress/index.html') 
 
     cy.get('.trigger').trigger('mouseover')
     cy.get('.trigger').trigger('mouseout')
-   
+})
+
+
+it.only('long bress command', ()=>{
+    cy.visit('cypress/index.html') 
+
+    cy.get('.trigger').trigger('mouseover')
+    cy.get('.trigger').trigger('mousedown')
+    cy.wait(5000)
+    cy.get('.trigger').trigger('mouseup')
 })
